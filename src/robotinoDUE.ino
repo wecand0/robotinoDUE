e96e53a3de1d802d0d51e8200b764d2aa54d82e5
Vadim <shorinvadim1@gmail.com>
2017-12-03 13:17:43 +0300

    Update robotinoDUE.ino

--- src/robotinoDUE.ino
+++ src/robotinoDUE.ino
@@ -1,3 +1,8 @@
+#include <CircleMode.h>
+#include <RobotinoDUE.h>
+
+#include <PID_v1.h>
+
 /*
    created: 25 nov. 2017
    by Vadim Shorin
@@ -29,7 +34,7 @@
 */
 
 
-#include <PID_v1.h>
+
 
 /*
     encoders (3 items)
@@ -92,118 +97,6 @@ void Motor2InterruptB();
 void Motor3InterruptA();
 void Motor3InterruptB();
 
-
-class Robotino
-{
-  public:
-    /*
-       prototypes of methods
-    */
-    Robotino();
-    void WhichDirectonHbridge1();
-    void WhichDirectonHbridge2();
-    void WhichDirectonHbridge3();
-    void SetVelocity(float radius, float inTime);  //Set velocity of driving
-    void SetOmega1(float inTime);                  //set omega1
-    void SetOmega2(float inTime);                  //set omega2
-    void SetOmega3(float inTime);                  //set omega3
-    double GetVelocity();
-    double GetOmega1();
-    double GetOmega2();
-    double GetOmega3();
-    bool GetDirection1();
-    bool GetDirection2();
-    bool GetDirection3();
-
-  private:
-    /*
-       vars after calculate
-    */
-    volatile int m_velocity; // velocity of robot
-    volatile int m_omega1;
-    volatile int m_omega2;
-    volatile int m_omega3;
-    volatile bool m_direction1;
-    volatile bool m_direction2;
-    volatile bool m_direction3;
-
-
-};
-
-/*
-   create object
-*/
-Robotino robotino; // global object, cause its arduino.. :(
-
-/*
-   //constructor set all vars to zero
-*/
-Robotino::Robotino():
-  m_velocity(0),
-  m_omega1(0),
-  m_omega2(0),
-  m_omega3(0),
-  m_direction1(0),
-  m_direction2(0),
-  m_direction3(0)
-{
-  /*
-     turn off all engines in start of program
-  */
-  analogWrite(s_pwmEngine1Fovard, 0);
-  analogWrite(s_pwmEngine1Back  , 0);
-  analogWrite(s_pwmEngine2Fovard, 0);
-  analogWrite(s_pwmEngine2Back  , 0);
-  analogWrite(s_pwmEngine3Fovard, 0);
-  analogWrite(s_pwmEngine3Back  , 0);
-}
-
-
-
-//////////////////////////////////
-///////////GET methods////////////
-/////////////////////////////////
-
-bool Robotino::GetDirection1()
-{
-  return m_direction1;
-}
-
-bool Robotino::GetDirection2()
-{
-  return m_direction2;
-}
-
-bool Robotino::GetDirection3()
-{
-  return m_direction3;
-}
-
-double Robotino::GetVelocity()
-{
-  return m_velocity;
-}
-
-double Robotino::GetOmega1()
-{
-  return m_omega1;
-}
-
-double Robotino::GetOmega2()
-{
-  return m_omega2;
-}
-
-double Robotino::GetOmega3()
-{
-  return m_omega3;
-}
-
-
-
-/////////////////////////////////
-///////////SET methods///////////
-/////////////////////////////////
 
 /*
    calc prm motor 1
@@ -293,85 +186,6 @@ void Motor3InterruptB()
   {
     motor3Count--;
   }
-}
-
-/*
-   choose the direction of motor 1
-*/
-void Robotino::WhichDirectonHbridge1() //less then zero -> false
-{
-  bool isWhichDirection = m_omega1 < 0 ? false : true;
-  if (isWhichDirection == true)
-  {
-    m_direction1 = true;
-  }
-  else if (isWhichDirection == false)
-  {
-    m_direction1 = false;
-  }
-}
-
-/*
-   choose the direction of motor 2
-*/
-void Robotino::WhichDirectonHbridge2() //less then zero -> false
-{
-  bool isWhichDirection = m_omega2 < 0 ? false : true;
-  if (isWhichDirection == true)
-  {
-    m_direction2 = true;
-  }
-  else if (isWhichDirection == false)
-  {
-    m_direction2 = false;
-  }
-}
-
-/*
-   choose the direction of motor 3
-*/
-void Robotino::WhichDirectonHbridge3() //less then zero -> false
-{
-  bool isWhichDirection = m_omega3 < 0 ? false : true;
-  if (isWhichDirection == true)
-  {
-    m_direction3 = true;
-  }
-  else if (isWhichDirection == false)
-  {
-    m_direction3 = false;
-  }
-}
-
-/*
-   calc velocity of robot to circle mode
-*/
-void Robotino::SetVelocity(float radius, float inTime)  //Set velocity of driving
-{
-  m_velocity = ((2 * PI * radius) / inTime);
-}
-
-/*
-   calc omega 1 to circle mode
-*/
-void Robotino::SetOmega1(float inTime) //set omega1
-{
-  m_omega1 = (((1 / s_RW) * (  (-m_velocity * s_sqrt3of2) + ((2 * PI * s_L) / inTime))) * s_PRM) * s_gain;
-}
-/*
-   calc omega 2 to circle mode
-*/
-void Robotino::SetOmega2(float inTime) //set omega2
-{
-  m_omega2 = ((((2 * PI * s_L) / (inTime)) / s_RW) * s_PRM) * s_gain;
-}
-
-/*
-   calc omega 3 to circle mode
-*/
-void Robotino::SetOmega3(float inTime) //set omega3
-{
-  m_omega3 = (((1 / s_RW) * (  (m_velocity * s_sqrt3of2) + ((2 * PI * s_L) / inTime))) * s_PRM) * s_gain;
 }
 
 /*
@@ -586,6 +400,5 @@ void BouncingMode()
 {
 
 }
-
 
 
